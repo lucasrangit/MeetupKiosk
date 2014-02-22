41884e30af3e61918b0e1449e43026a969e8245c
Lucas Rangit MAGASWERAN <lucasrangit@gmail.com>
2014-02-21 21:54:56 -0800

    ready for Scale 12x. changed API key to use Lance's. Changed refresh rate to 30 seconds to give time to capture QR code and read description. Tweaked the HTML for the group logos.

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from __future__ import with_statement
 import os
 import jinja2
 import webapp2
-import logging
+#import logging
 import urllib2
 import random
 import re
@@ -11,13 +11,10 @@ from time import localtime, strftime
 
 from lib import meetup_api_client as mac
 
-from secrets import MEETUP_API_KEY
+from secrets import MEETUP_API_KEY, MEETUP_USER_ID
 
 template_env = jinja2.Environment(
   loader=jinja2.FileSystemLoader(os.path.join(os.getcwd(),'templates')))
-
-#myuser ='lucasrangit'
-myid = 10705006
 
 # QR code max 177x177 px and 4,296 alphanumeric characters.
 qr_url = 'https://chart.googleapis.com/chart?cht=qr&choe=UTF-8&chs=150x150'
@@ -31,15 +28,15 @@ class MainPage(webapp2.RequestHandler):
   def get(self):
     mucli = mac.Meetup(MEETUP_API_KEY)
 
-    mygroups = mucli.get_groups(member_id=myid,text_format="plain")
+    mygroups = mucli.get_groups(member_id=MEETUP_USER_ID,text_format="plain")
     
     g = mygroups.results[random.randrange(0, mygroups.meta['total_count'])]
-    logging.info("Group ID: " + str(g.id))
-    logging.info(remove_img_tags(g.description))
+    #logging.info("Group ID: " + str(g.id))
+    #logging.info(remove_img_tags(g.description))
     
     if g.get_events(mucli,status="upcoming").meta['count'] > 0:
       e = g.get_events(mucli,status="upcoming",text_format="plain").results[0]
-      logging.info("Event ID: " + str(e.id))
+      #logging.info("Event ID: " + str(e.id))
       event_name = e.name
       event_datetime = strftime('%A %B %d', localtime(e.time/1000))
     else:
@@ -50,7 +47,6 @@ class MainPage(webapp2.RequestHandler):
       group_photo_link = g.group_photo['photo_link']
     else:
       group_photo_link = 'http://img2.meetupstatic.com/03115515559398589850/img/user_logos/meetup_logo_1.png'
-      #group_photo_link = '/static/keep-calm-and-meetup-dotcom.png'
     
     template = template_env.get_template('home.html')
     context = {
